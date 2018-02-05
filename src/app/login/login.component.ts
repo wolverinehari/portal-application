89eef733f4ce2da475a50220a0df9f3410ad852a
Subramanian <hari.html5@gmail.com>
2018-02-05 15:10:21 -0600

    Navbar removed

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,ViewEncapsulation } from '@angular/core';
 import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+    encapsulation:ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
 
