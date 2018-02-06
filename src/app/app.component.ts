0a3f80b88b8ff79759ba61843fbf7dce48f2279f
Subramanian <hari.html5@gmail.com>
2018-02-06 17:48:28 -0600

    Login scenario header handled

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
- // title = 'app';
- islogin:boolean=window.location.pathname.indexOf('login')>=0?true:false;
-  constructor(private route: ActivatedRoute) {
-    console.log(this.islogin);
+export class AppComponent {
+  // title = 'app';
+  islogin:boolean;
+ constructor(private route: ActivatedRoute, private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event.constructor.name === "NavigationStart") {
+        this.islogin = event.url.indexOf('login') >= 0 || event.url.length<=1 ? false : true;
+        console.log(this.islogin,event);
+      }
+    })
      //const id = +this.route.
   }
 }
